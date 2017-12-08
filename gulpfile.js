02a1f02c15118415cbcd291107afc0634dec84b8
hude <hugo.dewattinne@smals.be>
2017-12-08 10:27:44 +0100

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
-var browserSync = require('browser-sync').create();
+var browserSync = require('browser-sync');
+var runSequence = require('run-sequence');
+var del = require('del');
 var src = './src';
 var dist = './dist';
 
+// Start browserSync server
 gulp.task('browserSync', function() {
   browserSync({
     server: {
@@ -13,55 +16,113 @@ gulp.task('browserSync', function() {
   })
 })
 
+// Optimizing HTML
 gulp.task('html', function(){
     gulp.src([src + '/**/*.html', src + '/**/*.twig'])
-        .pipe(cleanhtml())
-        .pipe(gulp.dest('dist'));
+        .pipe(plugins.cleanhtml())
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
+// Optimizing CSS
 gulp.task('css', function () {
     return gulp.src(src + '/sass/*.scss')
-        .pipe(plugins.sass())
+        .pipe(plugins.sass().on('error', plugins.sass.logError))
         .pipe(plugins.csscomb())
-        /*.pipe(plugins.cssbeautify({indent: '  '})) Si le CSS ne doit pas être minifier*/
+        //.pipe(plugins.cssbeautify({indent: '  '})) Si le CSS ne doit pas être minifier
         .pipe(plugins.csso())
         .pipe(plugins.rename({
             suffix: '.min'
         }))
-        .pipe(gulp.dest(dist + '/css/'));
+        .pipe(gulp.dest(dist + '/css/'))
         .pipe(browserSync.reload({
             stream: true
-        }))
+        }));
 });
 
+// Optimizing JS
 gulp.task('js', function() {
-    return gulp.src(src + '/js/*.js')
-        .pipe(uglify())
-        .pipe(concat('scripts.min.js'))
-        .pipe(gulp.dest(dist + '/js/'));
+    return gulp.src(src + '/js/**/*.js')
+        .pipe(plugins.uglify())
+        //.pipe(plugins.concat('scripts.min.js'))
+        .pipe(gulp.dest(dist + '/js/'))
         .pipe(browserSync.reload({
             stream: true
-        }))
+        }));
 });
 
-gulp.task('img', function () {
+// Optimizing iamges
+gulp.task('images', function () {
     return gulp.src(src + '/images/**/*.{png,jpg,jpeg,gif,svg}')
-        .pipe(imagemin())
-        .pipe(gulp.dest(dist + '/images'));
+        .pipe(plugins.cache(plugins.imagemin({
+            interlaced: true
+        })))
         .pipe(browserSync.reload({
             stream: true
-        }))
+        }));
+});
+
+// Copying fonts
+gulp.task('fonts', function() {
+  return gulp.src(src + '/fonts/**/*')
+    .pipe(gulp.dest(dist + '/fonts'))
+});
+
+// Copying assets
+gulp.task('assets', function() {
+  return gulp.src(src + '/assets/**/*')
+    .pipe(gulp.dest(dist + '/assets'))
+});
+
+// Copying libraries
+gulp.task('vendor', function() {
+    gulp.src([
+        'node_modules/bootstrap/dist/js/*.min.js',
+        ])
+        .pipe(gulp.dest(dist + '/vendor/bootstrap'))
+
+    gulp.src([
+        'node_modules/jquery/dist/jquery.js',
+        'node_modules/jquery/dist/jquery.min.js'
+        ])
+        .pipe(gulp.dest(dist + '/vendor/jquery'))
+
+    gulp.src([
+            'node_modules/font-awesome/fonts/*',
+        ])
+        .pipe(gulp.dest(dist + '/vendor/font-awesome'))
+})
+
+// Cleaning 
+gulp.task('clean', function(callback) {
+  del(dist);
+  return plugins.cache.clearAll(callback);
+});
+
+gulp.task('clean:dist', function(callback){
+  del([dist + '/**/*', '!' + dist + '/images', '!' + dist + '/images/**/*'], callback)
 });
 
 // Tâche "build"
-gulp.task('build', ['html','css', 'js']);
+gulp.task('build',function(callback) {
+    runSequence(
+        'clean',
+        ['vendor','html', 'css', 'js', 'images', 'fonts', 'assets'],
+        callback
+    );
+});
 
-// Tâche "watch" = je surveille *less
-gulp.task('watch', ['browserSync', 'build'], function () {
+// Watchers
+gulp.task('watch', ['build', 'browserSync'], function () {
     gulp.watch(src + '**/*.html', ['html']);
     gulp.watch(src + '/sass/*/**.scss', ['css']);
-    gulp.watch(src + '/sass/*/**.scss', ['css']);
+    gulp.watch(src + '/js/*/**.js', ['js']);
+    gulp.watch(src + '/images/**/*', ['images']);
+    gulp.watch(src + '/fonts/**/*', ['fonts']);
+    gulp.watch(src + '/assets/**/*', ['assets']);
 });
 
 // Tâche par défaut
-gulp.task('default', ['build']);
+gulp.task('default', ['watch']);
